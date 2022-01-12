1fbc1884cc7cb0d5021d86e2f11abee7ec224afa
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2022-01-11 21:59:21 -0500

    implement and tested the warehouses routes

---
+++ app/controllers/api/v1/warehouses_controller.rb
@@ -0,0 +1,59 @@
+module Api
+  module V1
+    class WarehousesController < ApplicationController
+      skip_before_action :verify_authenticity_token
+      # GET /api/v1/warehouses
+      def index
+        warehouses = Warehouse.all
+
+        render json: WarehouseSerializer.new(warehouses).serializable_hash.to_json
+      end
+
+      # GET /api/v1/warehouses/:id
+      def show
+        warehouse = Warehouse.find_by(id: params[:id])
+
+        render json: WarehouseSerializer.new(warehouse).serializable_hash.to_json
+      end
+
+      # POST /api/v1/warehouses
+      def create
+        warehouse = Warehouse.new(warehouse_params)
+
+        if warehouse.save
+          render json: WarehouseSerializer.new(warehouse).serializable_hash.to_json
+        else
+          render json: { error: warehouse.errors.messages }, status: 422
+        end
+
+      end
+      
+      # PATCH /api/v1/warehouses/:id
+      def update
+        warehouse = Warehouse.find_by(id: params[:id])
+        
+        if warehouse.update(warehouse_params)
+          render json: WarehouseSerializer.new(warehouse).serializable_hash.to_json
+        else
+          render json: { error: warehouse.errors.messages }, status: 422
+        end
+      end
+
+      # DELETE /api/v1/warehouses/:id
+      def destroy
+        warehouse = Warehouse.find_by(id: params[:id])
+
+        if warehouse.destroy
+          head :no_content
+        else
+          render json: { error: warehouse.errros.messages }, status: 422
+        end
+      end
+
+      # private
+      def warehouse_params
+        params.require(:warehouse).permit(:name, :address)
+      end
+    end
+  end
+end
---
+++ spec/requests/api/v1/warehouses_spec.rb
@@ -0,0 +1,137 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/warehouses', type: :request do
+
+  path '/api/v1/warehouses' do
+
+    get('list warehouses') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create warehouse') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/warehouses/new' do
+
+    get('new warehouse') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/warehouses/{id}/edit' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('edit warehouse') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/warehouses/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show warehouse') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    patch('update warehouse') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    put('update warehouse') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete warehouse') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
